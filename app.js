1176b40ff7ec69653c2e38bc70f8c95ad5734c92
Chandrachud <66238180+chandrasahi10@users.noreply.github.com>
2023-07-26 12:18:02 +0530

    Used router

--- app.js
+++ app.js
@@ -18,10 +18,22 @@ app.use(express.static('public'));
 app.engine('hbs', expressHbs.engine( {extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
-
-
-app.get('', (req,res)=>{
-    res.render('home');
+const pool = mysql.createPool({
+    connectionLimit : 100,
+    host            : process.env.DB_HOST,
+    user            : process.env.DB_USER,
+    password        : process.env.DB_PASS,
+    database        : process.env.DB_NAME
 });
+
+pool.getConnection((err, connection)=>{
+    if(err){
+        throw err;
+    };
+    console.log('Connected as ID'+ connection.threadId);
+});
+
+const routes = require('./server/routes/user');
+app.use('/',routes);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
